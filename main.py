b8525f0b56f21edf8fdc0be040891ef0682e4701
sylvain hellin <hellinsylvain@gmail.com>
2025-05-08 19:49:02 +0200

    removed main function to better utilise REPL

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 # %%
-def main():
-    from smolagents import CodeAgent
-    from src.tools import TOOLS
-    from src.config import LANGUAGE_MODELS
 
-    model = LANGUAGE_MODELS["claude"]
+from smolagents import CodeAgent
+from src.tools import TOOLS
+from src.config import LANGUAGE_MODELS
 
-    agent = CodeAgent(
-        tools=TOOLS,
-        model=model,
-        additional_authorized_imports=[
-            "ifcopenshell",
-            "ifcopenshell.util.element",
-            "ifcopenshell.util.shape",
-            "ifcopenshell.util.placement",
-            "ifcopenshell.util.geolocation",
-            "ifcopenshell.util.system",
-            "ifcopenshell.geom",
-            "ifcopenshell.file",
-            "ifcopenshell.entity_instance",
-        ],
-    )
+model = LANGUAGE_MODELS["claude"]
 
-    agent.run(
-        "What is the height of the ceiling in room A203? Path to the .ifc file: /Users/sylvainhellin/GitHub/ifcAnswerEngineV3/src/bim_models/duplex/arc.ifc"
-    )
+agent = CodeAgent(
+    tools=TOOLS,
+    model=model,
+    additional_authorized_imports=[
+        "ifcopenshell",
+        "ifcopenshell.util.element",
+        "ifcopenshell.util.shape",
+        "ifcopenshell.util.placement",
+        "ifcopenshell.util.geolocation",
+        "ifcopenshell.util.system",
+        "ifcopenshell.geom",
+        "ifcopenshell.file",
+        "ifcopenshell.entity_instance",
+    ],
+)
 
-
-if __name__ == "__main__":
-    main()
+# agent.run(
+#     "What is the height of the ceiling in room A203? Path to the .ifc file: /Users/sylvainhellin/GitHub/ifcAnswerEngineV3/src/bim_models/duplex/arc.ifc"
+# )
 
 # %%
+from smolagents import GradioUI
 
-# %%
-
-# %%
-
-# %%
-
+GradioUI(agent).launch()
 # %%
