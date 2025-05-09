281c5c50f869bd7786551eec870b6345994b5861
sylvain hellin <hellinsylvain@gmail.com>
2025-05-09 15:08:27 +0200

    trying up stuff

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from smolagents import CodeAgent
 from src.tools import TOOLS
 from src.config import LANGUAGE_MODELS
 
-model = LANGUAGE_MODELS["claude"]
+model = LANGUAGE_MODELS["mistral"]
 
 agent = CodeAgent(
     tools=TOOLS,
@@ -20,14 +20,20 @@ agent = CodeAgent(
         "ifcopenshell.file",
         "ifcopenshell.entity_instance",
     ],
+    max_print_outputs_length=2048
 )
 
-# agent.run(
-#     "What is the height of the ceiling in room A203? Path to the .ifc file: /Users/sylvainhellin/GitHub/ifcAnswerEngineV3/src/bim_models/duplex/arc.ifc"
-# )
+agent.run(
+    "What is the height of the ceiling in room A203?",
+    max_steps=20,
+    additional_args={
+        "path_to_ifc_file": "/Users/sylvainhellin/GitHub/ifcAnswerEngineV3/src/bim_models/duplex/arc.ifc"
+    }
+)
+# TODO: use the additional_arg argument when calling agent.run() to pass more information (like model path, etc.)
 
-# %%
-from smolagents import GradioUI
+# # %%
+# from smolagents import GradioUI
 
-GradioUI(agent).launch()
-# %%
+# GradioUI(agent).launch()
+# # %%
