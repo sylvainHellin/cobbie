206a8549cfb21e5b42f1c092c4f14152a302b1a1
sylvain hellin <hellinsylvain@gmail.com>
2025-05-09 22:52:20 +0200

    improve modularity

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ register()
 SmolagentsInstrumentor().instrument()
 
 # Select LLM
-model = LANGUAGE_MODELS["mistral"]
+model = LANGUAGE_MODELS["claude"]
 
 # Set up the agent
 agent = CodeAgent(
@@ -28,12 +28,20 @@ agent = CodeAgent(
         "ifcopenshell.file",
         "ifcopenshell.entity_instance",
     ],
-    max_print_outputs_length=2048
+    max_print_outputs_length=2**12 # 4.096
 )
 
+# Define task
+# # GFA
+# task = "What is the total gross floor area of the building? Carefully review the definition of the GFA first, to be sure what areas need to be included or excluded in this calculation. Based on a rough calculation with an IFC viewer, it should be around 313 m² (156 m² per story, 2 stories)"
+
+# Room names + create tool at the end
+task = "Give me the name of all the rooms in the building, as well as a helper function that would enable me to get this information directly for another model next time."
+
+# Run the agent
 agent.run(
-    "What is the height of the ceiling in room A203?",
-    max_steps=20,
+    task=task,
+    max_steps=30,
     additional_args={
         "path_to_ifc_file": "/Users/sylvainhellin/GitHub/ifcAnswerEngineV3/src/bim_models/duplex/arc.ifc"
     }
