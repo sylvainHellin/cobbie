100501288cf92acae38a127e7bbf7f61da634dcc
sylvain hellin <hellinsylvain@gmail.com>
2025-05-15 13:20:48 +0200

    restructuring project as multi agent system

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# %%
-'''
+# %% Section::setup
+"""
 Current implementation of the IfcAnswerEngineV3.
 Trace can be seen on: https://cloud.langfuse.com/
-'''
+"""
 
 import base64
 import os
@@ -12,7 +12,7 @@ from openinference.instrumentation.smolagents import SmolagentsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
-from smolagents import CodeAgent
+from smolagents import (CodeAgent, tool)
 
 from src.config import LANGUAGE_MODELS
 from src.tools import TOOLS
@@ -36,7 +36,49 @@ SmolagentsInstrumentor().instrument(tracer_provider=trace_provider)
 
 # Select LLM
 model = LANGUAGE_MODELS["llama4_maverick"]
-model = LANGUAGE_MODELS["llama4_scout"]
+# model = LANGUAGE_MODELS["llama4_scout"]
+
+# %% Section::variables
+QUESTION = "What is the height of the ceiling in room A203?"
+GROUND_TRUTH = "The height of the ceiling in room A203 is 2.58 m."
+
+# %% Section::Managed agents
+
+# %% Subsection::answer_verifier
+
+# tools
+@tool
+def get_correct_answer(question_id: int) -> str:
+    """
+    Query the database and return the ground truth to the given question
+    
+    Args:
+        question_id: The ID of the question for which the ground truth is to be returned.
+        
+    Returns:
+        str: the correct answer
+    """
+    return GROUND_TRUTH
+
+
+answer_verifier = CodeAgent(
+    tools=[],
+    model=model,
+    name="answer_verifier",
+    description="Compare your answer with the correct answer to verify that it is correct. If it is incorrect, explain why."
+)
+
+# %% Section - Orchestrator agent
+TASK_ORCHESTRATOR = """
+Your task is to assess if you can answer the question using the existing tools and, if not, define the requirements to create a new tool.
+
+Here is how you should proceed:
+    1. Try to answer the question using a combination of the existing tools (as well as some code in between if useful)
+    2. If you are missing a tool to answer this question, you can call the tool_maker, who will give you a code snippet for the new tool.
+    3. Once you have all the tools you need, and you think you can answer the question, call the verify_answer agent. This agent has access to the ground truth, and will tell you if your answer is correct.
+        3.1 If the answer is correct, call the save_tool agent who will review the code snippet of the new tool, ensure it's format is compliant, and save it for future use.
+        3.2 If the answer is incorrect, use the provided feedback to try to come to the right answer. If you think the error comes from the new tool, you can call the tool_maker again and ask him to fix it.
+"""
 
 # Set up the agent
 agent = CodeAgent(
@@ -71,6 +113,7 @@ agent.run(
         "path_to_ifc_file": "/Users/sylvainhellin/GitHub/ifcAnswerEngineV3/src/bim_models/duplex/arc.ifc"
     }
 )
+
 # TODO: use the additional_arg argument when calling agent.run() to pass more information (like model path, etc.)
 
 # # %%
