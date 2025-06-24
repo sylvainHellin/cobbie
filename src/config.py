5ecc568610b4fb6c5effd2f42f976ee044965218
sylvain hellin <hellinsylvain@gmail.com>
2025-06-24 17:15:30 +0200

    updated path

--- src/config.py
+++ src/config.py
@@ -5,13 +5,13 @@ from typing import Literal
 
 load_dotenv(find_dotenv())
 
-# Get the src directory path
+# Path
 ROOT_PATH = os.environ["ROOT_PATH"]
 SRC_PATH = os.path.join(ROOT_PATH, "src")
-TEST_IFC_PATH = "src/experiment/bim_models/duplex/arc.ifc"
-# PATH
-DATASET_PATH = os.path.join(SRC_PATH, "experiment/datasets", "ifc-bench-v1.csv")
-VECTORSTORE_PATH = "src/engine/tools/primordial/vectorstore"
+TEST_IFC_PATH = os.path.join(SRC_PATH, "src/experiment/bim_models/duplex/arc.ifc")
+DATASET_PATH = os.path.join(SRC_PATH, "experiment/datasets", "ifc-bench-v1.1.csv")
+VECTORSTORE_PATH = os.path.join(ROOT_PATH, "src/engine/tools/primordial/vectorstore")
+DB_PATH = os.path.join(ROOT_PATH, "src/experiment/db/db.db")
 
 # Load the overview of the documentation of IfcOpenShell
 doc_path = os.path.join(
