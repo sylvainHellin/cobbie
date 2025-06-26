512a5420dff34dfb24d03c0a9bca54251d29163a
sylvain hellin <hellinsylvain@gmail.com>
2025-06-26 17:22:30 +0200

    Update __init__.py

---
+++ src/engine/__init__.py
@@ -0,0 +1,5 @@
+from .engine import IfcAnswerEngine
+from .components.create_new_tool import ToolCreator
+from .components.extract_function_name import NameExtractor
+
+__all__ = ["IfcAnswerEngine", "ToolCreator", "NameExtractor"]
