8f761b06253079cdb56263f4bfc13704a7b0b69a
sylvain hellin <hellinsylvain@gmail.com>
2025-07-10 23:32:18 +0200

    new runs

--- src/engine/components/__init__.py
+++ src/engine/components/__init__.py
@@ -1,4 +1,5 @@
 from .code_act import CodeAct
 from .tool_creator import ToolCreator
+from .extract_function_name import NameExtractor
 
-__all__ = ["CodeAct", "ToolCreator"]
+__all__ = ["CodeAct", "ToolCreator", "NameExtractor"]
