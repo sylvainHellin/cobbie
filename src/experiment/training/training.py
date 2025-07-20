4be0634795537c930a5193dd2eefd318211783a8
sylvain hellin <hellinsylvain@gmail.com>
2025-07-20 13:40:18 +0100

    WIP - not tested yet

--- src/experiment/training/training.py
+++ src/experiment/training/training.py
@@ -1,4 +1,5 @@
 from typing import Literal, Optional
+import time
 
 import dspy
 import mlflow
@@ -6,7 +7,7 @@ import mlflow
 from src.config import LANGUAGE_MODELS, LOG_LEVEL
 from src.engine import IfcAnswerEngine, NameExtractor, ToolCreator
 from src.engine.schemas import ModuleOutput
-from src.engine.util import get_logger
+from src.engine.util import get_logger, save_new_tool
 from src.experiment.evaluation.answer_verifier import AnswerVerifier
 
 from .data_loader import Datapoint, load_train_dev_split
@@ -15,31 +16,64 @@ from .data_loader import Datapoint, load_train_dev_split
 class TrainingModule(dspy.Module):
     def __init__(
         self,
-        llm: dspy.LM,
+        lm: dspy.LM,
         log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = LOG_LEVEL,
         training_size: Optional[int] = 2,
         similarity_treshold: float = 0.8,
+        add_code_prefix = True,
+        max_retry_engine: int = 2,
+        max_iter_engine: int = 10,
+        max_tokens_logs: int = 2**12,
+        max_tokens_outputs: int = 2**12,
+        import_all_created_tools: bool = True,
+        tracking_uri: str = "http://127.0.0.1:5000",
+        experiment_name: str = "Training"
     ):
         super().__init__()
-        self.llm = llm
+        self.lm = lm
         self.log_level = log_level
-        self.logger = get_logger(name="TrainingModule", log_level=log_level)
+        self.logger = get_logger(name="Training", log_level=log_level)
         self.training_size = training_size
         self.similarity_treshold = similarity_treshold
+        self.add_code_prefix = add_code_prefix
+        self.max_retry_engine = max_retry_engine
+        self.max_iter_engine = max_iter_engine
+        self.max_tokens_logs = max_tokens_logs
+        self.max_tokens_outputs = max_tokens_outputs
+        self.import_all_created_tools = import_all_created_tools
+        self.tracking_uri = tracking_uri
+        self.experiment_name = experiment_name
         self.correct_answer: Optional[bool] = None
         self.new_function_created: Optional[bool] = None
         self.function_name: Optional[str] = None
         self.function_code: Optional[str] = None
         self.function_requirements: Optional[str] = None
-        self.tool_creator = ToolCreator(llm=self.llm)
         self.answer_verifier = AnswerVerifier()
-        self.name_extractor = NameExtractor()
-        self.engine = IfcAnswerEngine(llm=self.llm)
+
+        self.engine = IfcAnswerEngine(
+            llm=self.lm,
+            max_retry=self.max_retry_engine,
+            max_iters=self.max_iter_engine,
+            log_level=self.log_level,
+            max_tokens_logs=self.max_tokens_logs,
+            max_tokens_output=self.max_tokens_outputs,
+            import_all_created_tools=self.import_all_created_tools,
+            add_code_prefix=self.add_code_prefix
+            )
 
     def forward(self, datapoint: Datapoint) -> ModuleOutput:
-        output = ModuleOutput(status="error")
 
-        with mlflow.start_span("Training"):
+        # Set-up MLflow and DSPy
+        mlflow.dspy.autolog()  # type: ignore
+        mlflow.set_tracking_uri(self.tracking_uri)
+        mlflow.set_experiment(self.experiment_name)
+        dspy.configure(lm=self.lm)
+
+        # Instantiate the original output
+        output = ModuleOutput(status="error", error_msg="Initial error msg from Training Module.")
+
+        now = time.time()
+        with mlflow.start_span(str(now)):
             # Init and run engine
             output_engine = self.engine.forward(
                 question=datapoint.question, path_ifc_model=datapoint.ifc_model_path
@@ -49,11 +83,11 @@ class TrainingModule(dspy.Module):
             if output_engine.status == "success":
                 # Assert that our logic is sound.
                 assert output_engine.result.need_new_function is not None, (
-                    "Something is off: the status is set to 'success', but the field `need_new_function` is None."
+                    "Something is off: the status is set to 'success', but the field `need_new_function` is None. Should be True or False."
                 )
 
                 assert output_engine.result.answer is not None, (
-                    "Something is off: the status is set to 'success', but the `answer` field is None."
+                    "Something is off: the status is set to 'success', but the `answer` field is None. It should contain something."
                 )
 
                 # If the Engine could answer the question, let's check if it's correct
@@ -88,58 +122,18 @@ class TrainingModule(dspy.Module):
                         self.logger.info(
                             f"Similarity score: \n{output_answer_verifier.result.similarity_score}\nCorrect answer: {correct_answer}"
                         )
-                # If the Engine need a new function, let's try to create it!
-                else:
-                    self.function_requirements = output_engine.result.answer
-                    self.logger.info("A new tool is needed to answer this question.")
-                    self.logger.debug(
-                        f"Requirements for the new tool:\n{self.function_requirements}\n"
-                    )
-                    # 1. Extract the name of the function
-                    output_name_extractor = self.name_extractor.forward(
-                        function_requirements=self.function_requirements
-                    )
+                        # If the Engine created a new function, let's see if it's correctly implemented.
+                        if correct_answer and output_engine.result.need_new_function:
+                            new_function_name = output_engine.result.function_name
+                            new_function_implementation = output_engine.result.function_implementation
+                            assert new_function_name, "Logical Error: The output of the engine is a success, and it needs a new function, but the function name is None."
+                            assert(new_function_implementation), "Logical Error: the output of the engine is a success and it needed a new function, but the function implementation is None."
 
-                    if output_name_extractor.status == "error":
-                        self.logger.error(
-                            f"Error when trying to extract the function's name: {output_name_extractor.error_msg}"
-                        )
-                    else:
-                        assert output_name_extractor.result.function_name is not None, (
-                            "Something is off: the status of NameExtractor is 'success', but the `function_name` field is None."
-                        )
-                        self.function_name = output_name_extractor.result.function_name
-                        self.logger.info("Name of the function extracted successfully.")
-                        self.logger.debug(
-                            f"Name of the function: {self.function_name}\n"
-                        )
-
-                        # Start the ToolCreator!
-                        output_tool_creator = self.tool_creator.forward(
-                            function_requirements=self.function_requirements,
-                            function_name=self.function_name,
-                            path_ifc_model=datapoint.ifc_model_path,
-                        )
-
-                        if output_tool_creator.status == "error":
-                            self.logger.error(
-                                f"Error during Tool Creation: {output_tool_creator.error_msg}"
+                            new_tool_saved = save_new_tool(
+                                function_name=new_function_name,
+                                function_implementation=new_function_implementation,
                             )
-                        else:
-                            assert (
-                                output_tool_creator.result.function_implementation
-                                is not None
-                            ), (
-                                "Something is off: the status of ToolCreator is 'success', but the `python_code` field is None."
-                            )
-                            self.function_code = (
-                                output_tool_creator.result.function_implementation
-                            )
-                            self.logger.info("Tool was successfully created.")
-                            self.logger.debug(
-                                f"Python code of the function: {self.function_name}:\n{self.function_code}"
-                            )
-                            # TODO Continue here
+                            assert new_tool_saved, "Could not save the new tool."
 
             else:
                 self.logger.error(
@@ -149,7 +143,7 @@ class TrainingModule(dspy.Module):
 
 
 def main(
-    lm_name: str = "llama4-maverick-groq",
+    lm_name: str = "kimi-k2",
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "DEBUG",
     training_size: Optional[int] = 2,
     similarity_treshold: float = 0.8,
@@ -176,7 +170,7 @@ def main(
         # Log info
         logger.info(f"Try to answer new question: {datapoint.question}\n\n")
         mlflow.start_run(run_name=f"question_id_{datapoint.id}")
-        training_module = TrainingModule(llm=llm)
+        training_module = TrainingModule(lm=llm)
         output = training_module.forward(datapoint=datapoint)
         print(output)
 
