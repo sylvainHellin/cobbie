8a197b19fc39353b771d25248c5ce749adf4399a
sylvain hellin <hellinsylvain@gmail.com>
2025-07-23 13:20:27 +0200

    added save_new_tool

--- src/engine/util/__init__.py
+++ src/engine/util/__init__.py
@@ -4,6 +4,7 @@ from .get_logger import get_logger
 from .validate_type import validate_type
 from .check_final_answer import check_final_answer
 from .create_code_prefix import create_code_prefix
+from .save_new_tool import save_new_tool
 
 __all__ = [
     "get_logger",
@@ -12,5 +13,5 @@ __all__ = [
     "validate_type",
     "check_final_answer",
     "create_code_prefix",
-    "save_new_tool"
+    "save_new_tool",
 ]
