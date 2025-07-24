217a81ea8a0850141e02866ebe423cbff6f5c36c
sylvain hellin <hellinsylvain@gmail.com>
2025-07-24 20:54:27 +0200

    added helper function to create chat history

--- src/engine/schemas/chat.py
+++ src/engine/schemas/chat.py
@@ -1,6 +1,6 @@
 import json
 from pydantic import BaseModel
-from typing import List, Literal, Optional
+from typing import List, Literal, Optional, Dict, TypeGuard
 
 
 class Message(BaseModel):
@@ -19,3 +19,16 @@ class Chat(BaseModel):
         dump = [msg.model_dump_json(indent=2) for msg in self.messages]
 
         print("\n---\n".join(dump))
+
+    def import_chat_messages(self, messages: List[Dict[str, str]]):
+        def is_valid_role(
+            role: str,
+        ) -> TypeGuard[Literal["system", "user", "assistant"]]:
+            return role in ["system", "user", "assistant"]
+
+        for entry in messages:
+            role = entry.get("role", "")
+
+            if is_valid_role(role):
+                msg = Message(role=role, content=entry.get("content", ""))
+                self.append_msg(msg=msg)
