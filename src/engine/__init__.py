761c41c764e6b74f4e3fe271715626391198df26
sylvain hellin <hellinsylvain@gmail.com>
2025-07-25 21:40:04 +0200

    added ToolIdentifier

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,5 +1,6 @@
 from .engine import IfcAnswerEngine
 from .components.tool_creator import ToolCreator
 from .components.extract_function_name import NameExtractor
+from .components.tool_identifier import ToolIdentifier
 
-__all__ = ["IfcAnswerEngine", "ToolCreator", "NameExtractor"]
+__all__ = ["IfcAnswerEngine", "ToolCreator", "NameExtractor", "ToolIdentifier"]
