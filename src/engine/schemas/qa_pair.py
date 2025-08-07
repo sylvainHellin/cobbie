a5ee235b11aa0bede02833e7fea1ee3ecaf1185b
sylvain hellin <hellinsylvain@gmail.com>
2025-08-07 10:32:20 +0200

    new method to convert to dspy.Example

--- src/engine/schemas/qa_pair.py
+++ src/engine/schemas/qa_pair.py
@@ -1,7 +1,8 @@
+from pydantic import BaseModel
 from dspy import Example
 
 
-class QA_Pair(Example):
+class QA_Pair(BaseModel):
     id: int
     question: str
     answer: str
@@ -9,3 +10,19 @@ class QA_Pair(Example):
     ifc_model_name: str
     ifc_model_path: str
     ifc_model_description: str
+
+    def to_example(self) -> Example:
+        """
+        Transform the custom type into dspy.Example for dspy optimization.
+        """
+        example = Example(
+            question=self.question,
+            answer=self.answer,
+            path_ifc_model=self.ifc_model_path,
+            question_id=self.id,
+        ).with_inputs(
+            "question",
+            "path_ifc_model",
+        )
+
+        return example
