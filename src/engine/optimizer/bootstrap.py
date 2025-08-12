6082166a23425002f50e853f8ea2ede5c13c0e0f
sylvain hellin <hellinsylvain@gmail.com>
2025-08-12 21:37:31 +0200

    Update bootstrap.py

--- src/engine/optimizer/bootstrap.py
+++ src/engine/optimizer/bootstrap.py
@@ -3,43 +3,63 @@ from dspy import LM, BootstrapFewShot
 
 from src.config import LANGUAGE_MODELS, PATH_COMPILED_MODEL
 from src.engine import IfcAnswerEngine
-from src.experiment import TRAINSET, DEVSET, metric
+from src.experiment import TRAINSET, metric
 
-mlflow.dspy.autolog(log_evals=True, log_compiles=True)  # type: ignore
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment(experiment_name="Optimizer")
 
-bootstrap_optimizer = BootstrapFewShot(
-    max_bootstrapped_demos=3,
-    max_labeled_demos=2**4,
-    metric=metric,
-    metric_threshold=0.9,
-    max_rounds=3,
-    max_errors=5,
-)
+def bootstrap_engine(
+    engine: IfcAnswerEngine,
+    save: bool = True,
+) -> IfcAnswerEngine:
+    bootstrap_optimizer = BootstrapFewShot(
+        max_bootstrapped_demos=3,
+        max_labeled_demos=2**4,
+        metric=metric,
+        metric_threshold=0.9,
+        max_rounds=3,
+        max_errors=5,
+    )
 
-teacher_llm = LANGUAGE_MODELS["openrouter-claude"]
-teacher_llm = LM(
-    model=teacher_llm.url,
-    api_key=teacher_llm.api_key,
-    max_tokens=2**14,
-)
-student_llm = LANGUAGE_MODELS["openrouter-gpt-oss-120b"]
-student_llm = LM(
-    model=student_llm.url,
-    api_key=student_llm.api_key,
-    max_tokens=2**14,
-)
-teacher = IfcAnswerEngine(llm=teacher_llm)
-student = IfcAnswerEngine(llm=student_llm)
+    teacher_llm = LANGUAGE_MODELS["openrouter-claude"]
+    teacher_llm = LM(
+        model=teacher_llm.url,
+        api_key=teacher_llm.api_key,
+        max_tokens=2**14,
+    )
+    student_llm = LANGUAGE_MODELS["openrouter-gpt-oss-120b"]
+    student_llm = LM(
+        model=student_llm.url,
+        api_key=student_llm.api_key,
+        max_tokens=2**14,
+    )
 
-trainset = [qa.to_example() for qa in TRAINSET][10:20]
+    teacher = IfcAnswerEngine(llm=teacher_llm)
 
-with mlflow.start_span(name="BootstrapFewShort"):
+    trainset = [qa.to_example() for qa in TRAINSET]
+
     optimized_engine = bootstrap_optimizer.compile(
-        student=student,
+        student=engine,
         teacher=teacher,
         trainset=trainset,
     )
 
-    optimized_engine.save(PATH_COMPILED_MODEL)
+    if save:
+        optimized_engine.save(PATH_COMPILED_MODEL)
+
+    return optimized_engine
+
+
+if __name__ == "__main__":
+    mlflow.dspy.autolog(log_evals=True, log_compiles=True)  # type: ignore
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment(experiment_name="Optimizer")
+
+    with mlflow.start_span(name="BootstrapFewShort"):
+        student_llm = LANGUAGE_MODELS["openrouter-gpt-oss-120b"]
+        student_llm = LM(
+            model=student_llm.url,
+            api_key=student_llm.api_key,
+            max_tokens=2**14,
+        )
+        engine = IfcAnswerEngine(llm=student_llm)
+
+        bootstrap_engine(engine=engine)
