a1addcedc254dfd2e44a9e6642486bb795488ca4
sylvain hellin <hellinsylvain@gmail.com>
2025-09-11 14:54:52 +0200

    disabled cache for testing

--- src/engine/components/tool_debugger.py
+++ src/engine/components/tool_debugger.py
@@ -182,6 +182,7 @@ if __name__ == "__main__":
         mlflow.set_tracking_uri("http://127.0.0.1:5000")
         mlflow.set_experiment("ToolDebugger")
 
+        dspy.configure_cache(enable_disk_cache=False)
         # setup the tool debugger
         tool_debugger = ToolDebugger()
 
