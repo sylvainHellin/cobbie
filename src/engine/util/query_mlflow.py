f8549d6112809529549797ad73242c7f498c1e91
sylvain hellin <hellinsylvain@gmail.com>
2025-09-22 13:38:10 +0200

    WIP - extract the info from MLFlow for analysis

--- src/engine/util/query_mlflow.py
+++ src/engine/util/query_mlflow.py
@@ -1,109 +1,129 @@
+from typing import List, Optional, Tuple, Dict
 import json
-import sys
-import os
-from typing import Literal, Optional, List, Tuple
 
-from dotenv import load_dotenv, find_dotenv
 import mlflow
 from mlflow import MlflowClient
-from mlflow.entities import Span, Experiment, Run, Trace
-
-load_dotenv(find_dotenv())
-ROOT_PATH = os.getenv("ROOT_PATH", "")
-sys.path.append(ROOT_PATH)
-
-from src.config import MLFLOW_URI
-from src.engine.schemas import Chat, Message
-
-mlflow.set_tracking_uri(MLFLOW_URI)
-client = MlflowClient()
-
-experiment_name = "Training"
-run_name = "2025-07-24-09-17-10"
-experiment = client.get_experiment_by_name(name=experiment_name)
-assert experiment is not None, (
-    f"Couldn't find an experiment with this name: {experiment_name}"
-)
-runs = client.search_runs(
-    experiment_ids=[experiment.experiment_id],
-)
+from mlflow.entities import Experiment, Run, Trace, RunData
 
 
-def get_experiment_by_name(name: str) -> Optional[Experiment]:
-    return client.get_experiment_by_name(name=name)
+class CustomMLFlowClient(MlflowClient):
+    run: Optional[Run] = None
+    runData: Optional[RunData] = None
+    experiment: Optional[Experiment] = None
+    traces: Optional[List[Trace]] = None
+    metrics: Optional[Dict] = None
+
+    def set_experiment_by_name(self, name: str):
+        self.experiment = self.get_experiment_by_name(name=name)
+
+    def get_runs(
+        self,
+    ) -> List[Run]:
+        runs = []
+        if self.experiment is not None:
+            paged_list = self.search_runs(
+                experiment_ids=[self.experiment.experiment_id]
+            )
+            runs = [run for run in paged_list]
+        return runs
+
+    def set_run_by_name(
+        self,
+        name: str,
+    ) -> Optional[Tuple[int, Run]]:
+        runs = self.get_runs()
+        for run in runs:
+            if name == (run.data.tags.get("mlflow.runName")):
+                self.run = run
+                self.runData = run.data
+                self.metrics = run.data.metrics
+
+    def setup(
+        self,
+        experiment_name: str,
+        run_name: str,
+    ):
+        self.set_experiment_by_name(name=experiment_name)
+        self.set_run_by_name(name=run_name)
+        self.set_traces()
+
+    def dump_run_metrics(self):
+        print(json.dumps(getattr(self.runData, "metrics", {}), indent=2))
+
+    def set_traces(self) -> None:
+        self.traces = []
+
+        if self.experiment is not None and self.run is not None:
+            for trace in self.search_traces(
+                experiment_ids=[self.experiment.experiment_id],
+                run_id=self.run.info.run_id,
+            ):
+                self.traces.append(trace)
+
+    # def get_spans(
+    #     trace: Trace,
+    #     span_type: Optional[
+    #         Literal[
+    #             "TOOL",
+    #             "CHAIN",
+    #             "PARSER",
+    #             "UNKNOWN",
+    #             "CHAT_MODEL",
+    #             "LLM",
+    #             "MODULE",
+    #             "QUESTION",
+    #         ]
+    #     ],
+    # ) -> List[Span]:
+    #     spans = [span for span in trace.data.spans]
+
+    #     if span_type:
+    #         filtered_spans = []
+
+    #         for span in spans:
+    #             spanType = span.attributes["mlflow.spanType"]
+    #             if spanType == span_type:
+    #                 filtered_spans.append(span)
+
+    #         return filtered_spans
+
+    #     else:
+    #         return spans
+
+    # def get_chat_messages(span: Span) -> Chat:
+    #     chat = Chat()
+    #     if span.attributes["mlflow.spanType"] == "CHAT_MODEL":
+    #         for msg in span.attributes["mlflow.chat.messages"]:
+    #             role = msg["role"]
+    #             content = msg["content"]
+    #             chat.append_msg(Message(role=role, content=content))
+    #     return chat
 
 
-def get_runs(
-    experiment: Experiment,
-) -> List[Run]:
-    paged_list = client.search_runs(experiment_ids=[experiment.experiment_id])
-    output = [run for run in paged_list]
-    return output
+if __name__ == "__main__":
+    from src.config import MLFLOW_URI
 
+    mlflow.set_tracking_uri(MLFLOW_URI)
+    client = CustomMLFlowClient()
 
-def get_run(
-    runs: List[Run],
-    run_name: str,
-) -> Optional[Tuple[int, Run]]:
-    for run in runs:
-        if run_name == (run.data.tags.get("mlflow.runName")):
-            return (run.info.run_id, run)
+    # Define paramenters
+    experiment_name = "Evaluation"
+    run_name = "2025-09-22-09-36-54"
 
+    # Setup the client
+    client.setup(
+        experiment_name=experiment_name,
+        run_name=run_name,
+    )
 
-def get_traces(
-    experiment: Experiment,
-    run_id: Optional[str],
-) -> List[Trace]:
-    traces = [
-        trace
-        for trace in client.search_traces(
-            experiment_ids=[experiment.experiment_id],
-            run_id=run_id,
-        )
-    ]
-
-    return traces
-
-
-def get_spans(
-    trace: Trace,
-    span_type: Optional[
-        Literal[
-            "TOOL",
-            "CHAIN",
-            "PARSER",
-            "UNKNOWN",
-            "CHAT_MODEL",
-            "LLM",
-            "MODULE",
-            "QUESTION",
-        ]
-    ],
-) -> List[Span]:
-    spans = [span for span in trace.data.spans]
-
-    if span_type:
-        filtered_spans = []
-
-        for span in spans:
-            spanType = span.attributes["mlflow.spanType"]
-            if spanType == span_type:
-                filtered_spans.append(span)
-
-        return filtered_spans
-
-    else:
-        return spans
-
-
-def get_chat_messages(span: Span) -> Chat:
-    chat = Chat()
-    if span.attributes["mlflow.spanType"] == "CHAT_MODEL":
-        for msg in span.attributes["mlflow.chat.messages"]:
-            role = msg["role"]
-            content = msg["content"]
-            chat.append_msg(Message(role=role, content=content))
-    return chat
+    # print(client.experiment)
+    # print(client.run)
+    client.dump_run_metrics()
+    for trace in client.traces[:1] if client.traces is not None else []:
+        print("\n#### Trace info ####\n")
+        print(json.dumps(trace.info.to_dict(), indent=2))
+        # print("\n#### Trace data ####\n")
+        # print(trace.data)
 
 
 # TODO Continue here
