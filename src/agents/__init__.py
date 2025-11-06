95f8e667757e4082ae37e11276e6b438e470dd70
sylvain hellin <hellinsylvain@gmail.com>
2025-11-06 10:32:44 +0100

    easier import of agents

---
+++ src/agents/__init__.py
@@ -0,0 +1,11 @@
+from src.agents.answer_verifier import verify_answer
+from src.agents.cobbie import cobbie
+from src.agents.create_helper_function import create_helper_function
+from src.agents.identify_helper_function import identify_helper_function
+
+__all__ = [
+    "cobbie",
+    "verify_answer",
+    "identify_helper_function",
+    "create_helper_function",
+]
