15ffbd139e104ac8db45d75f73c97e32a94fa9d0
sylvain hellin <hellinsylvain@gmail.com>
2025-11-06 10:02:09 +0100

    placeholder for the training phase orchestration

---
+++ scripts/run_training_phase.py
@@ -0,0 +1,3 @@
+"""
+The orchestration of the multi-agent system that generates, updates and prunes the tools for extracting information from BIM models. These tools are then used by Cobbie at inference time.
+"""
